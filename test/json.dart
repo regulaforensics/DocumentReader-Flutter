8e22052e66ce6ecad5cda2c093be2a3e4e56ac6b
Jenkins <jenkins@regula.by>
2026-06-23 13:07:44 +0300

    9.6.1345-rc

--- test/json.dart
+++ test/json.dart
@@ -250,6 +250,7 @@ var customizationMatrices = {
   "nextPageIdCardBack": [2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9],
 };
 var customization = {
+  "theme": 1,
   "showStatusMessages": true,
   "showResultStatusMessages": false,
   "showHelpAnimation": true,
