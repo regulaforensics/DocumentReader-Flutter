4a48d91e375bdb94f40c31e9ddbc42618726622a
Jenkins <jenkins@regula.by>
2026-07-29 17:51:38 +0300

    9.7.1538-rc

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -56,7 +56,7 @@ extensions.configure<KotlinAndroidProjectExtension> {
 }
 
 dependencies {
-    implementation("com.regula.documentreader:api:9.7.13170") {
+    implementation("com.regula.documentreader:api:9.7.13172") {
         isTransitive = true
     }
 
--- ios/flutter_document_reader_api/Package.swift
+++ ios/flutter_document_reader_api/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .package(name: "FlutterFramework", path: "../FlutterFramework"),
         .package(
             url: "https://github.com/regulaforensics/DocumentReader-Swift-Package",
-            exact: "9.7.6768-stage"
+            exact: "9.7.6776-stage"
         ),
     ],
     targets: [
