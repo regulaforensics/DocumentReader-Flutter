28d42b710f9b778de6b292cec0272e5a5a58c198
Jenkins <jenkins@regula.by>
2026-08-07 01:27:36 +0300

    9.8.1578-nightly

--- ios/flutter_document_reader_api/Package.swift
+++ ios/flutter_document_reader_api/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .package(name: "FlutterFramework", path: "../FlutterFramework"),
         .package(
             url: "https://github.com/regulaforensics/DocumentReader-Swift-Package",
-            exact: "9.8.6821-nightly"
+            exact: "9.8.6822-nightly"
         ),
     ],
     targets: [
