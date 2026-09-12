8fb8dffbfe553c3782b6f820c3e674fbea56faec
Jenkins <jenkins@regula.by>
2026-09-12 04:53:27 +0300

    9.9.1737-rc

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -56,7 +56,7 @@ extensions.configure<KotlinAndroidProjectExtension> {
 }
 
 dependencies {
-    implementation("com.regula.documentreader:api:9.8.13336") {
+    implementation("com.regula.documentreader:api:9.9.13348") {
         isTransitive = true
     }
 
